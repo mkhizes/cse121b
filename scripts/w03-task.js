464cce7ae3dd47cbbb252d9dca8fd8e6041f6bf6
mkhizes <lubanzi01trading@gmail.com>
2024-02-04 21:25:52 +0200

    added some changes

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -28,42 +28,42 @@ document.querySelector('#subtractNumbers').addEventListener('click', subtractNum
 const multiply=(factor1,factor2)=>factor1*factor2;
 const multiplyNumbers=()=>{
 
-const factor1=parseFloat(document.getElementById('factor1').value);
-const factor2=parseFloat(document.getElementById('factor2').value);
+const factor1=parseFloat(document.getElementById('#factor1').value);
+const factor2=parseFloat(document.getElementById('#factor2').value);
 
 if (! isNaN(factor1) && !isNaN(factor2)){
 
     const product=multiply(factor1, factor2);
 
-document.getElementById('product').textContent=`Product: ${product}`; 
+document.getElementById('#product').textContent=`Product: ${product}`; 
 } else {   
-document.getElementById('product').textContent='Enter numbers';
+document.getElementById('#product').textContent='Enter numbers';
 }
 }
-document.getElementById('multiplyNumbers').addEventListener('çlick', multiplyNumbers);
+document.getElementById('#multiplyNumbers').addEventListener('çlick', multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
 const divide=(x,y)=>x/y;
 
 const divideNumbers =(x,y)=>{
-    let dividend= parseFloat(document.getElementById("dividend").value);
-    let divisor= parseFloat(document.getElementById("divisor").value);
+    let dividend= parseFloat(document.getElementById("#dividend").value);
+    let divisor= parseFloat(document.getElementById("#divisor").value);
     let quotient=divide(dividend, divisor);
-    document.getElementById("quotient").value=quotient.toFixed(2);
+    document.getElementById("#quotient").value=quotient.toFixed(2);
 };
-document.getElementById("divideNumbers").addEventListener("click",divideNumbers);
+document.getElementById("#divideNumbers").addEventListener("click",divideNumbers);
 
 
 /* Decision Structure */
 function getTotal(){
 
-let subtotal=parseFloat(document.getElementById('subtotal').value);
-if (document.getElementById('member').checked){
+let subtotal=parseFloat(document.getElementById('#subtotal').value);
+if (document.getElementById('#member').checked){
     total=subtotal-subtotal*0.2;
 }
-document.getElementById('total'),texContent=total.toFixed(2);
+document.getElementById('#total'),texContent=total.toFixed(2);
 }
-document.getElementById('getTotal').addEventListener('click',getTotal);
+document.getElementById('#getTotal').addEventListener('click',getTotal);
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
@@ -74,23 +74,23 @@ document.getElementById("array").textContent = `Array: ${numbersArray.join(", ")
 
 /* Output Odds Only Array */
 const oddNumbers = numbersArray.filter(number => number % 2 !== 0);
-document.getElementById("odds").textContent = `Odds: ${oddNumbers.join(", ")}`;
+document.getElementById("#odds").textContent = `Odds: ${oddNumbers.join(", ")}`;
 
 /* Output Evens Only Array */
 const evenNumbers = numbersArray.filter(number => number % 2 === 0);
-document.getElementById("evens").textContent = `Evens: ${evenNumbers.join(", ")}`;
+document.getElementById("#evens").textContent = `Evens: ${evenNumbers.join(", ")}`;
 
 /* Output Sum Only Array */
 const sumOfArray = numbersArray.reduce((sum, number) => sum + number, 0);
-document.getElementById("sumOfArray").textContent = `Sum of Array: ${sumOfArray}`;
+document.getElementById("#sumOfArray").textContent = `Sum of Array: ${sumOfArray}`;
 
 /* Output Multiplied by 2 Only Array */
 const multipliedArray = numbersArray.map(number => number * 2);
-document.getElementById("multiplied").textContent = `Multiplied by 2: ${multipliedArray.join(", ")}`;
+document.getElementById("#multiplied").textContent = `Multiplied by 2: ${multipliedArray.join(", ")}`;
 
 /* Output sumOfMultiplied by 2 Only Array */
 const sumOfMultiplied = numbersArray.map(number => number * 2).reduce((sum, number) => sum + number, 0);
-document.getElementById("sumOfMultiplied").textContent = `Sum of Multiplied: ${sumOfMultiplied}`;
+document.getElementById("#sumOfMultiplied").textContent = `Sum of Multiplied: ${sumOfMultiplied}`;
 
 
 
